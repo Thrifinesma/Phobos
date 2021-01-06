7ca80424170ca195b0d7750cbaded104b1fadb90
Belonit <Belonit@ya.ru>
2021-01-06 19:29:15 +0300

    Small refactoring for BlitterFixes

--- src/Misc/BlitterFixes.cpp
+++ src/Misc/BlitterFixes.cpp
@@ -1,7 +1,5 @@
 #include <Helpers/Macro.h>
-#include "../Utilities/Debug.h"
-
-#define ushort unsigned __int16
+#include <stdint.h>
 
 // Half of an RGB range in 16 bit
 // (equivalent of (127, 127, 127))
@@ -9,30 +7,36 @@
 
 // Blending mask for 16-bit pixels
 // https://medium.com/@luc.trudeau/fast-averaging-of-high-color-16-bit-pixels-cb4ac7fd1488
+
 #define BLENDING_MASK 0xF7DEu
 
-inline ushort Blit50TranslucencyFix(ushort dst, ushort src)
+inline uint16_t  Blit50TranslucencyFix(uint16_t  dst, uint16_t  src)
 {
-	return (((((src) ^ (dst)) & BLENDING_MASK) >> 1) + ((src) & (dst)));
+	return (((src ^ dst) & BLENDING_MASK) >> 1) + (src & dst);
 }
 
-inline ushort Blit75TranslucencyFix(ushort dst, ushort src)
+inline uint16_t  Blit75TranslucencyFix(uint16_t  dst, uint16_t  src)
 {
-	ushort div = (((((src) ^ (dst)) & BLENDING_MASK) >> 1) + ((src) & (dst)));
+	uint16_t  div = Blit50TranslucencyFix(dst, src);
 	return (div >> 1 & HALF_RANGE_MASK) + (dst >> 1 & HALF_RANGE_MASK);
 }
 
 //same as 75, just reversed order of args
-inline ushort Blit25TranslucencyFix(ushort dst, ushort src)
+inline uint16_t  Blit25TranslucencyFix(uint16_t  dst, uint16_t  src)
 {
 	return Blit75TranslucencyFix(src, dst);
 }
 
+#undef HALF_RANGE_MASK
+#undef BLENDING_MASK
+
+// =============================
+// container hooks
 
 DEFINE_HOOK(492866, BlitTransLucent50_Fix, 0)
 {
-	GET(ushort, color, EAX);
-	GET(ushort*, dest, EDI);
+	GET(uint16_t , color, EAX);
+	GET(uint16_t *, dest, EDI);
 
 	*dest = Blit50TranslucencyFix(*dest, color);
 
@@ -41,8 +45,8 @@ DEFINE_HOOK(492866, BlitTransLucent50_Fix, 0)
 
 DEFINE_HOOK(492956, BlitTransLucent25_Fix, 0)
 {
-	GET(ushort, color, EAX);
-	GET(ushort*, dest, ESI);
+	GET(uint16_t , color, EAX);
+	GET(uint16_t *, dest, ESI);
 
 	*dest = Blit25TranslucencyFix(*dest, color);
 
@@ -51,8 +55,8 @@ DEFINE_HOOK(492956, BlitTransLucent25_Fix, 0)
 
 DEFINE_HOOK(492776, BlitTransLucent75_Fix, 0)
 {
-	GET(ushort, color, EBP);
-	GET(ushort*, dest, ESI);
+	GET(uint16_t , color, EBP);
+	GET(uint16_t *, dest, ESI);
 
 	*dest = Blit75TranslucencyFix(*dest, color);
 
