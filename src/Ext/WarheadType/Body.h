338f128cec7d296148b3b27740997c3385970119
secsome <302702960@qq.com>
2021-03-17 14:08:36 +0800

    WarheadType

--- src/Ext/WarheadType/Body.h
+++ src/Ext/WarheadType/Body.h
@@ -39,21 +39,33 @@ public:
 			RemoveMindControl_ApplyCellSpread(true)
 		{ }
 
-		virtual void LoadFromINIFile(CCINIClass* pINI) override;
 		virtual ~ExtData() = default;
 
-		virtual void InvalidatePointer(void* ptr, bool bRemoved) override {}
+		virtual void Initialize() override;
 
-		virtual void LoadFromStream(IStream* Stm);
+		virtual void LoadFromINIFile(CCINIClass* pINI) override;
 
-		virtual void SaveToStream(IStream* Stm) const;
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) override {
+		}
+
+		virtual void LoadFromStream(PhobosStreamReader& Stm) override;
+
+		virtual void SaveToStream(PhobosStreamWriter& Stm) override;
+	
+	private:
+		template <typename T>
+		void Serialize(T& Stm);
 	};
 
 	class ExtContainer final : public Container<WarheadTypeExt> {
 	public:
 		ExtContainer();
 		~ExtContainer();
+
+		virtual void InvalidatePointer(void* ptr, bool bRemoved) override;
 	};
 
 	static ExtContainer ExtMap;
+	static bool LoadGlobals(PhobosStreamReader& Stm);
+	static bool SaveGlobals(PhobosStreamWriter& Stm);
 };
